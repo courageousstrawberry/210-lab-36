29c0514b7f519c3932ed571d16934d69e1c4f32e
courageousstrawberry <elech2a@gmail.com>
2024-12-01 11:44:02 -0800

    finalize main menu

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 void displayMenu() {
     cout << "Menu:\n";
-    cout << "1. Add a record\n";
-    cout << "2. Delete a record\n";
-    cout << "3. Search for a record\n";
-    cout << "4. Modify a record\n";
-    cout << "6. Exit\n";
+    cout << "1. Add a code\n";
+    cout << "2. Delete a code\n";
+    cout << "3. Search for a code\n";
+    cout << "4. Modify a code\n";
+    cout << "5. Exit\n";
 }
 
 int main() {
@@ -30,16 +30,51 @@ int main() {
 
     inputFile.close();
 
-    while(input != 6){
-        displayMenu();
-        cout << "Enter your choice: ";
-        cin >> input;
+    displayMenu();
+    cout << "Enter your choice: ";
+    cin >> input;
+
+    while(input != 5){
+        cin.ignore();
         switch(input) {
             case 1: {
-                cout << "Enter a code to add: "
+                cout << "Enter a code to add: ";
+                getline(cin, code);
+                tree.insertNode(code);
+                break;
+            }
+            case 2: {
+                cout << "Enter a code to delete: ";
+                getline(cin, code);
+                tree.remove(code);
+                break;
+            }
+            case 3: {
+                cout << "Enter a code to search for: ";
+                getline(cin, code);
+                if(tree.searchNode(code)){
+                    cout << "Code found." << endl;
+                }
+                else {
+                    cout << "Code not found." << endl;
+                }
+                break;
+            }
+            case 4: {
+                string oldCode, newCode;
+                cout << "Enter a code to modify: ";
+                getline(cin, oldCode);
+                cout << "Enter a new code to enter: ";
+                getline(cin, newCode);
+                tree.remove(oldCode);
+                tree.insertNode(newCode);
+                break;
             }
 
         }
+        displayMenu();
+        cout << "Enter your choice: ";
+        cin >> input;
     }
     return 0;
 }
