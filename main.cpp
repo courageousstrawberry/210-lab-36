a63bb2caa0d011556abccb6ae57fbda5e7e3b087
courageousstrawberry <elech2a@gmail.com>
2024-12-01 11:16:29 -0800

    change class and read from file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <fstream>
 #include "IntBinaryTree.h"
 using namespace std;
 
 int main() {
-    
+    IntBinaryTree tree;
+    ifstream inputFile("codes.txt");
+    string code;
+
+    if (!inputFile) {
+        cerr << "Error opening file." << endl;
+        return 1;
+    }
+
+    while(getline(inputFile, code)) {
+        tree.insertNode(code);
+    }
+
+    inputFile.close();
+
+    tree.displayInOrder();
+    cout << endl;
 
     return 0;
 }
